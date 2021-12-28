e03b50d8edb4a8fd4cb0486dc3c7b068012b2832
이두산 <sxfc675@gmail.com>
2021-12-28 17:11:25 +0900

    [21.12.28] 조건에 따른 매핑지정

--- JPA_ORM_03/src/main/java/jpaorm/Member.java
+++ JPA_ORM_03/src/main/java/jpaorm/Member.java
@@ -1,8 +1,22 @@
 package jpaorm;
 
-@Entity // 클래스를 테이블과 매핑한다고 JPA에게 알려준다. @Entity가 사용된 클래스를 엔티티 클래스라고 한다.
-@Table(name = "MEMBER") // 엔티티 클래스에 매핑할 테이블 정보를 알려준다.
-// 여기서는 name 속성을 사용해 Member 엔티티를 MEMBER 테이블에 매핑한다.
+import javax.persistence.*;
+import java.util.Date;
+
+/*
+객체와 테이블 매핑 => @Entity @Table
+기본 키 매핑 => @Id
+필드와 컬럼 매핑 => @Column
+연관관계 매핑 => @ManyToOne @JoinColumn
+
+다음과 같은 조건 추가
+1. 회원은 일반 회원과 관리자로 구분해야 한다.
+2. 회원 가입일과 수정일이 있어야 한다.
+3. 회원을 설면할 수 있는 필드가 있으며 필드 길이는 제한이 없다.
+*/
+
+@Entity
+@Table(name = "MEMBER")
 public class Member {
 
     @Id // primary key
@@ -12,11 +26,61 @@ public class Member {
     @Column(name = "NAME")
     private String username;
 
-    // 매핑정보가 없는 필드
-    // age 필드에는 매핑 어노테이션이 없다. 이렇게 매핑 어노테이션을 생략하면
-    // 필드명을 사용해서 컬럼명으로 매핑한다.
     private Integer age;
 
+    // 조건에 따른 추가부분
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+    /*
+    자바의 enum을 사용해서 회원의 타입을 구분했다. 이처럼 자바의 enum을 사용하려면 @Enumerated 어노테이션으로 매핑해야한다.
+    */
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+    /*
+    자바의 날짜 타입은 @Temporal을 사용해서 매핑한다.
+    */
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    @Lob
+    private String description;
+
+    // getter setter 추가
+    public RoleType getRoleType() {
+        return roleType;
+    }
+
+    public void setRoleType(RoleType roleType) {
+        this.roleType = roleType;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Date getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    // 기존 getter setter
     public String getId() {
         return id;
     }
---
+++ JPA_ORM_03/src/main/java/jpaorm/RoleType.java
@@ -0,0 +1,4 @@
+package jpaorm;
+
+public class RoleType {
+}
