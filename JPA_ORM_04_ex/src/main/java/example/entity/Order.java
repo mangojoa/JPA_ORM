0338f83370ef4cef7ce613c5521134e4ef773d94
이두산 <sxfc675@gmail.com>
2021-12-29 14:35:09 +0900

    [21.12.28] 데이터베이스 중심의 객체지향 설계 예제

---
+++ JPA_ORM_04_ex/src/main/java/example/entity/Order.java
@@ -0,0 +1,54 @@
+package example.entity;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Table(name = "ORDERS")
+public class Order {
+
+    @Id @GeneratedValue
+    @Column(name = "ORDER_ID")
+    private Long id;
+
+    @Column(name = "MEMBER_ID")
+    private Long memberid;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date orderDate;
+
+    @Enumerated(EnumType.STRING)
+    private OrderStatus status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getMemberid() {
+        return memberid;
+    }
+
+    public void setMemberid(Long memberid) {
+        this.memberid = memberid;
+    }
+
+    public Date getOrderDate() {
+        return orderDate;
+    }
+
+    public void setOrderDate(Date orderDate) {
+        this.orderDate = orderDate;
+    }
+
+    public OrderStatus getStatus() {
+        return status;
+    }
+
+    public void setStatus(OrderStatus status) {
+        this.status = status;
+    }
+}
---
+++ JPA_ORM_04_ex/src/main/java/example/entity/OrderStatus.java
@@ -0,0 +1,5 @@
+package example.entity;
+
+public enum OrderStatus {
+    ORDER, CANCEL
+}
