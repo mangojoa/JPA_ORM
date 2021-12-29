b4bb9d641d49e926655efad08987a5903a8f88e9
이두산 <sxfc675@gmail.com>
2021-12-29 13:17:49 +0900

    [21.12.28] 데이터베이스 어노테이션

--- JPA_ORM_03/src/main/java/jpaorm/Member.java
+++ JPA_ORM_03/src/main/java/jpaorm/Member.java
@@ -13,17 +13,30 @@ import java.util.Date;
 1. 회원은 일반 회원과 관리자로 구분해야 한다.
 2. 회원 가입일과 수정일이 있어야 한다.
 3. 회원을 설면할 수 있는 필드가 있으며 필드 길이는 제한이 없다.
+
+IDENTITY 전략
+기본 키 생성을 에이터베이스에 위임하는 전략이다. auto_increment 기능은 데이터 베이스가 기본 키를 자동으로 생성해준다.
+identity 전략을 사용하려면 @GeneratedValue의 strategy 속겅 값을 GeneratedType.IDENTITY로 지정하면 된다.
+
+SEQUENCE 전략
+유일한 값을 순서대로 생성하는 특별한 데이터베이스 오브젝트이다.
+@GeneratedValue(strategy = GenerationType.SEQUENCE,
+                generator = "sequence_name")
 */
 
 @Entity
-@Table(name = "MEMBER")
+@Table(name = "MEMBER", uniqueConstraints = {@UniqueConstraint(
+        name = "NAME_AGE_UNIQUE",
+        columnNames = {"NAME", "AGE"} )}) // 유니크 제약조건 설정
 public class Member {
 
     @Id // primary key
+    @GeneratedValue(strategy = GenerationType.IDENTITY // 이렇게 함으로써 identity 전략을 사용할 수 있다.
+    )
     @Column(name = "ID")
     private String id;
 
-    @Column(name = "NAME")
+    @Column(name = "NAME", nullable = false, length = 10) // DDL 생성기능 (table column의 설정을 할 수 있는 기능)
     private String username;
 
     private Integer age;
