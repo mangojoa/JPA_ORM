75517d454da25e916380fcdd41d339e3f9ef83ee
이두산 <sxfc675@gmail.com>
2022-01-05 11:57:13 +0900

    [22.01.05] 일대다 양방향 매핑의 방법과 단점 미해결

--- JPA_ORM_07/src/main/java/example02/Member.java
+++ JPA_ORM_07/src/main/java/example02/Member.java
@@ -12,6 +12,23 @@ public class Member {
 
     private String username;
 
+    @ManyToOne
+    @JoinColumn(name = "TEAM_ID", insertable = false, updatable = false)
+    /* [22.01.05] 일대다 양방향 매핑
+    @JoinColumn(name = "TEAM_ID")을 사용하기에 문제가 발생한다.
+    그렇기에 읽기만 가능하도록 insertable, updatable = false 설정을 해야한다.
+    하지만 고질적인 문제였던 일대다 단방향 매핑이 가지는 단점을 그대로 가지기에 되도록 다대일 양방향 매핑을 이용하자
+    */
+    private Team team;
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
+    }
+
     public Long getId() {
         return id;
     }
