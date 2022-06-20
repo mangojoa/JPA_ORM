023810838cd5f87827d71a0b9d6ceed89f702e4c
이두산 <sxfc675@gmail.com>
2022-06-20 20:30:33 +0900

    [22.06.20] 엔티티의 생명주기

--- JPA_Step_03/src/main/java/jpabook/start/JpaMain.java
+++ JPA_Step_03/src/main/java/jpabook/start/JpaMain.java
@@ -43,7 +43,17 @@ public class JpaMain {
     }
 
     private static void logic(EntityManager em) {
+        /* [22.06.20] 엔티티의 생명주기
+        * 비영속(new/transient) : 영속성 컨텍스트와 전혀 관계가 없는 상태
+        * 영속(managed) : 영속성 컨텍스트에 저장된 상태
+        * 준영속(detached) : 영속성 컨텍스트에 저장되었다가 분리된 상태
+        * 삭제(removed) : 삭제된 상태
+        * */
 
+        /* [22.06.20] 비영속
+        * 엔티티 객체를 생성했다. 지금은 순수한 객체 상태이며 아직 저장하지 않은 산태이다.
+        * 따라서 이는 영속성 컨텍스트와 관련이 없는 단계이다.
+        * */
         String id = "mangojoa";
         Member member = new Member();
         member.setId(id);
@@ -60,6 +70,11 @@ public class JpaMain {
         * 하지만 영속성 컨텍스트는 엔티티 매니저를 생성할 때 하나 만들어진다.
         * 그리고 엔티티 매니저를 통해서 영속성 컨텍스트에 접슨할 수 있고, 영속성 컨텍스트를 관리할 수 있다.
         * */
+
+        /* [22.06.20] 영속
+        * 엔티티 매니저를 통해서 엔티티를 영속성 컨텍스트에 저장했다. 이렇게 영속성 컨텍스트가 관리하는 엔티티를 영속 상태라 한다.
+        * 이제 회원 엔티티는 비영속 상태에서 영속 상태가 되었다. 결국 영속 상태라는 것은 영속성 컨텍스트에 의해 관리된다는 뜻이다.
+        * */
         em.persist(member);
 
         member.setAge(20);
@@ -67,12 +82,21 @@ public class JpaMain {
         Member findMember = em.find(Member.class, id);
         System.out.println("findMember = " + findMember.getUsername() + ", age = " + findMember.getAge());
 
-
         List<Member> members =
                 em.createQuery("select m from Member m", Member.class).getResultList();
         System.out.println("member.size = " + members.size());
 
-//        em.remove(member);
+        /* [22.06.20] 준영속
+        * 영속성 컨텍스트가 관리하던 영속 상태의 엔티티를 영속성 컨텍스트가 관리하지 않으면 준영속 상태가 된다.
+        * 특정 엔티티를 준영속 상태로 만들려면 em.detach()를 호출래서 영속성 컨텍스트를 닫거나 em.clear()를 호출해서 영속성 컨텍스트를 초기화해도
+        * 영속성 컨텍스트가 관리하던 엔티티는 준영속 상태가 된다.
+        * */
+        // em.detach(member);
+
+        /* [22.06.20] 삭제
+        * 엔티티를 영속성 컨텍스트와 데이터베이스에서 삭제한다.
+        * */
+        // em.remove(member);
     }
 
 }
