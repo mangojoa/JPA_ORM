7caa94d52f844099fd0f94ce242a399347033339
이두산 <sxfc675@gmail.com>
2022-06-20 20:12:56 +0900

    [22.06.20] EntityManagerFactory / EntityManager 의 개념

--- JPA_Step_03/src/main/java/jpabook/start/JpaMain.java
+++ JPA_Step_03/src/main/java/jpabook/start/JpaMain.java
@@ -6,10 +6,25 @@ import java.util.List;
 public class JpaMain {
 
     public static void main(String[] args) {
-
+        /* [22.06.20] 엔티티 매니저 팩토리
+        * EntityManagerFactory 를 생성하는 코드이다.
+        * 하지만 이는 생성하는데 비용이 매우 많이 들기에 처음에 한번만 생성하는 것이 좋다.
+        *
+        * 나아가 이는 여러 스레드가 동시에 접근해도 안전하다.
+        * 그러므로 다른 스레드 간에 공유해도 된다.
+        *
+        * EntityManagerFactory 는 어디서 정보를 가져오는건가 ?
+        * 이는 persistence.xml의 jpabook에서 호출한다.
+        * */
         EntityManagerFactory emf =
                 Persistence.createEntityManagerFactory("jpabool");
 
+        /* [22.06.20] 엔티티 매니저
+        * EntityManager는 생성하는 비용은 거의 들지 않는다.
+        *
+        * 이는 EntityManagerFactory와는 달리 여러 스레드가 동시에 접근하면
+        * 동시성 문제가 발생하므로 스레드 간에 절대 공유하면 안 된다.
+        * */
         EntityManager em = emf.createEntityManager();
 
 
