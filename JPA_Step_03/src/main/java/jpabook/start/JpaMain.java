74110f00ab78553841632707cc5d2d40032ba8ec
이두산 <sxfc675@gmail.com>
2022-06-20 19:25:29 +0900

    [22.06.20] JpaMain / Member 파일 셋팅

--- JPA_Step_03/src/main/java/jpabook/start/JpaMain.java
+++ JPA_Step_03/src/main/java/jpabook/start/JpaMain.java
@@ -1,4 +1,53 @@
 package jpabook.start;
 
+import javax.persistence.*;
+import java.util.List;
+
 public class JpaMain {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf =
+                Persistence.createEntityManagerFactory("jpabool");
+
+        EntityManager em = emf.createEntityManager();
+
+
+        EntityTransaction tx = em.getTransaction();
+
+        try {
+            tx.begin();
+            logic(em);
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+
+    private static void logic(EntityManager em) {
+
+        String id = "mangojoa";
+        Member member = new Member();
+        member.setId(id);
+        member.setUsername("mango Platation");
+        member.setAge(28);
+
+        em.persist(member);
+
+        member.setAge(20);
+
+        Member findMember = em.find(Member.class, id);
+        System.out.println("findMember = " + findMember.getUsername() + ", age = " + findMember.getAge());
+
+
+        List<Member> members =
+                em.createQuery("select m from Member m", Member.class).getResultList();
+        System.out.println("member.size = " + members.size());
+
+//        em.remove(member);
+    }
+
 }
