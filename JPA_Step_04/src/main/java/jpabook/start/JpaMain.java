94ff6f61d409494118acaff669f35741d3dedbf7
이두산 <sxfc675@gmail.com>
2022-07-26 20:30:04 +0900

    [22.07.26] persistence.xml 속성 추가

---
+++ JPA_Step_04/src/main/java/jpabook/start/JpaMain.java
@@ -0,0 +1,55 @@
+package jpabook.start;
+
+import javax.persistence.*;
+import java.util.List;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf =
+                Persistence.createEntityManagerFactory("jpabook");
+
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+
+        try {
+            tx.begin();
+            logic(em);
+            tx.commit();
+        } catch (Exception e) {
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+        emf.close();
+    }
+
+    private static void logic(EntityManager em) {
+
+        String id = "망고따는누나";
+
+        Member member = new Member();
+        member.setId(id);
+        member.setUsername("망고플랜테이션");
+        member.setAge(28);
+
+        em.persist(member);
+
+        member.setAge(20);
+
+        Member findMember = em.find(Member.class, id);
+
+        System.out.println("findMember = " + findMember.getUsername() + ", age = " + findMember.getAge());
+
+        List<Member> members =
+                em.createQuery("select m from Member m", Member.class).getResultList();
+        System.out.println("member.size = " + members.size());
+
+        // em.detach(member);
+
+        // em.remove(member);
+    }
+
+}
